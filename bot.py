324e3a87a6dc29bcc3e88aca5d1c54877b978226
Mario Álvarez <ahoy@m4grio.me>
2017-09-23 15:04:11 +0800

    Imports optimizados

--- bot.py
+++ bot.py
@@ -1,25 +1,28 @@
-import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler, API, Stream
 
 CONSUMER_KEY = 'tWO7XoirfPqQo8RdeYM8SvwbD'
 CONSUMER_SECRET = '9T503l9pKTaZPu3j40Nd1Hpew1YXlgqvPqHuVVRUYPSkZephCT'
 ACCESS_KEY = '911451685058379776-Ei9QMSQgBhhJrAn955RSdH90BtEnC4z'
 ACCESS_SECRET = 'K12iw5WDAas2Dp57oEUXaObQVL4fEGt3YPiiusZLfGlF1'
 
-with open('sources/retweets') as f:
-    lines = f.readlines()
-accounts_to_track = [x.strip() for x in lines]
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+twitter_client = API(auth)
 
-class Listener(tweepy.StreamListener):
+class Listener(StreamListener):
     def on_status(self, status):
+        # twitter_client.retweet(status['id'])
         print(status.text)
     def on_error(self, status_code):
         if status_code == 420:
             return False
 
-stream_listener = Listener()
-stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.filter(follow=accounts_to_track, async=True)
+if __name__ == '__main__':
+    with open('sources/retweets') as f:
+        lines = f.readlines()
+        accounts_to_track = [x.strip() for x in lines]
+    
+    stream_listener = Listener()
+    stream = Stream(auth=twitter_client.auth, listener=stream_listener)
+    stream.filter(follow=accounts_to_track, async=True)
