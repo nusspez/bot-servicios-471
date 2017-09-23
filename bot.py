28a15fb86112f0b66e60a5aeb27fe539d8c7640f
Mario Álvarez <ahoy@m4grio.me>
2017-09-23 19:27:22 +0800

    Ya funciona pero aún no se puede usar; hay que ignorar varias cosas

--- bot.py
+++ bot.py
@@ -1,28 +1,21 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, API, Stream
+from config import config
 
-CONSUMER_KEY = 'tWO7XoirfPqQo8RdeYM8SvwbD'
-CONSUMER_SECRET = '9T503l9pKTaZPu3j40Nd1Hpew1YXlgqvPqHuVVRUYPSkZephCT'
-ACCESS_KEY = '911451685058379776-Ei9QMSQgBhhJrAn955RSdH90BtEnC4z'
-ACCESS_SECRET = 'K12iw5WDAas2Dp57oEUXaObQVL4fEGt3YPiiusZLfGlF1'
-
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-twitter_client = API(auth)
+auth_config = config.get('auth')
+auth = OAuthHandler(auth_config.get('consumer_key'), auth_config.get('consumer_secret'))
+auth.set_access_token(auth_config.get('access_key'), auth_config.get('access_secret'))
+client = API(auth)
 
 class Listener(StreamListener):
     def on_status(self, status):
-        # twitter_client.retweet(status['id'])
+        # client.retweet(status['id'])
         print(status.text)
     def on_error(self, status_code):
         if status_code == 420:
             return False
 
 if __name__ == '__main__':
-    with open('sources/retweets') as f:
-        lines = f.readlines()
-        accounts_to_track = [x.strip() for x in lines]
-    
     stream_listener = Listener()
-    stream = Stream(auth=twitter_client.auth, listener=stream_listener)
-    stream.filter(follow=accounts_to_track, async=True)
+    stream = Stream(auth=client.auth, listener=stream_listener)
+    stream.filter(follow=[d.get('id') for d in config.get('retweet').get('from')], async=True)
