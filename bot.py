2c4099f78381df58afe292ffe020b8146f80af67
Mario Álvarez <ahoy@m4grio.me>
2017-09-23 19:37:41 +0800

    Ignorando tuits que no fueron creados por las cuentas que queremos darles RT

--- bot.py
+++ bot.py
@@ -7,10 +7,13 @@ auth = OAuthHandler(auth_config.get('consumer_key'), auth_config.get('consumer_s
 auth.set_access_token(auth_config.get('access_key'), auth_config.get('access_secret'))
 client = API(auth)
 
+accounts_to_follow = [d.get('id') for d in config.get('retweet').get('from')]
+
 class Listener(StreamListener):
     def on_status(self, status):
-        # client.retweet(status['id'])
-        print(status.text)
+        if status.author.id_str in accounts_to_follow:
+            # client.retweet(status['id'])
+            print(status.text)
     def on_error(self, status_code):
         if status_code == 420:
             return False
@@ -18,4 +21,4 @@ class Listener(StreamListener):
 if __name__ == '__main__':
     stream_listener = Listener()
     stream = Stream(auth=client.auth, listener=stream_listener)
-    stream.filter(follow=[d.get('id') for d in config.get('retweet').get('from')], async=True)
+    stream.filter(follow=accounts_to_follow, async=True)
