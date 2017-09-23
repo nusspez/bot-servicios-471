dc8d66b89ecf376af9a186fea564f3a4a8f3eb86
Mario Álvarez <ahoy@m4grio.me>
2017-09-23 19:49:29 +0800

    Removed auth stuff from the public and a bit of a cleanup

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, API, Stream
 from config import config
+import os
 
-auth_config = config.get('auth')
-auth = OAuthHandler(auth_config.get('consumer_key'), auth_config.get('consumer_secret'))
-auth.set_access_token(auth_config.get('access_key'), auth_config.get('access_secret'))
+auth = OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
+auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
+
 client = API(auth)
 
 accounts_to_follow = [d.get('id') for d in config.get('retweet').get('from')]
 
+
 class Listener(StreamListener):
+    
     def on_status(self, status):
         if status.author.id_str in accounts_to_follow:
             # client.retweet(status['id'])
             print(status.text)
+    
     def on_error(self, status_code):
         if status_code == 420:
             return False
+
 
 if __name__ == '__main__':
     stream_listener = Listener()
